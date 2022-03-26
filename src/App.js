ee4c420cea8efac911d88768b10dcfde8ed1d824
pbcarloz <pb_carlos@outlook.com>
2022-03-25 22:09:12 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      name: 'Carlos',
+      age: 29,
+    };
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello {this.state.name} Your age is {this.state.age} </p>
+          <button 
+            onClick={() => {
+              this.setState({ name: 'Miki', age: 28});
+            }}
+          >
+          Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
